f1dbb7c257c42cb9c72ad62affd0af74d845a55a
mfahmialif <mfahmialif@gmail.com>
2022-11-03 13:03:56 +0700

    update publishes

--- src/LTEServiceProvider.php
+++ src/LTEServiceProvider.php
@@ -10,7 +10,7 @@ class LTEServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/views' => resource_path('views/layouts/lte'),
+            __DIR__ . '/views' => resource_path('views'),
             __DIR__ . '/public' => public_path('admin'),
         ]);
     }
